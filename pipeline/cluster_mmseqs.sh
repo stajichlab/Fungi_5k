c8c1948f804a493c68ab3fd60822b6eeed912ca4
Jason Stajich <jasonstajich.phd@gmail.com>
2024-09-02 19:58:06 -0700

    MMseqs clustering

---
+++ pipeline/cluster_mmseqs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash -l
+#SBATCH -p epyc -N 1 -n 48 --mem 384gb  --out logs/mmseqs.%A.log
+
+# Load modules
+module load mmseqs2
+module load workspace/scratch
+PREFIX=Fungi5K
+mkdir -p $SCRATCH/mmseqs
+DB=$SCRATCH/mmseqs/$PREFIX
+mkdir -p results
+time mmseqs createdb input/*.proteins.fa $DB --compressed 1 --dbtype 2
+#mmseqs cluster $DB ${DB}_cluster $SCRATCH --compressed 1 --cluster-reassign 1
+time mmseqs linclust $DB ${DB}_cluster $SCRATCH --compressed 1 
+
+mmseqs createtsv $DB $DB ${DB}_cluster results/${PREFIX}_linclust.tsv.gz --compressed 1
---
+++ pipeline/process_mmseqs_clusters.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash -l
+#SBATCH -p epyc --mem 96g -N 1 -n 1 -c 2 --out logs/mmseqs2pairwise.log
+
+module load workspace/scratch
+
+filecount=$(ls -U input | wc -l | awk '{print $1}')
+filecount=$(($filecount + 100))
+if [[ $filecount -gt 1024 ]]; then
+	ulimit -n $filecount
+fi
+pigz -dc results/Fungi5K_linclust.tsv.gz | time python scripts/mmseqs2pairwise.py --seqs input -o results/pairwise_MMseq_orthologs -v 
