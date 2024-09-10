0c3f7183d3c918574c1490dc45302f4c32805d1a
Jason Stajich <jasonstajich.phd@gmail.com>
2024-09-10 09:36:41 -0700

    add debugging

--- scripts/mmseqs2pairwise.py
+++ scripts/mmseqs2pairwise.py
@@ -7,7 +7,7 @@ import re
 import os
 import time
 import gzip
-import bsddb3
+import plyvel
 
 def main():
     parser = argparse.ArgumentParser(
@@ -22,21 +22,25 @@ def main():
     parser.add_argument('--seqs','--fasta', help='Directory to link gene names to species', 
                         type=pathlib.Path, required=True)
     parser.add_argument('--prefix', help='Cluster Name prefix', default="MMCLUST")
-    parser.add_argument('--tmpdir','--tmp', help='Temporary Directory for local DB', default="./tmp")
+    parser.add_argument('--tmpdir','--tmp', help='Temporary Directory for local DB')
     parser.add_argument('-v','--debug', help='Debugging output', action='store_true')
     
     args = parser.parse_args()
     
     if os.path.exists(args.outdir):
-        print(f"Output directory {args.outdir} already exists. Will overwrite existing files.")
+        print(f"Output directory {args.outdir} already exists. Will overwrite existing files.",file=sys.stderr)
     else:
         os.mkdir(args.outdir)
-    if not os.path.exists(args.tmpdir):
-        os.mkdir(args.tmpdir)
 
     # open directory and read seq files
+    # Create or open the LevelDB database
+    db = None
+    if args.tmpdir:
+        db = plyvel.DB(args.tmpdir, create_if_missing=True)
     gene2species = {}
     species_gene_count = {}
+
+    # Read in the gene files and populate the database
     filepat = re.compile(r'(\S+)\.(fa|faa|fasta|seq|aa|pep|cds|nt|dna)')    
     t0 = time.time()
     for file in os.listdir(args.seqs):
@@ -45,17 +49,22 @@ def main():
             species = fp.group(1)
             species = species.replace('.final','')   # deal with extra suffixes
             species = species.replace('.proteins','')   # deal with extra suffixes
-            # assume this will be okay match
-            # t3 = time.time()
+#            if args.debug:
+#                t3 = time.time()
             with open(os.path.join(args.seqs,file),'r') as f:
                 for line in f:                    
                     if line.startswith('>'):
                         gene = line.split()[0][1:]
-                        gene2species[gene] = species
-                        species_gene_count[species] = species_gene_count.get(species,0) + 1
-            # t4 = time.time()
-            # if args.debug:
-            #    print(f"Reading {file} took {t4-t3} seconds",file=sys.stderr)
+                        if db:
+                            # Store the gene-species mapping in the database
+                            db.put(gene.encode(), species.encode())
+                        else:
+                            gene2species[gene] = species
+                        species_gene_count[species] = species_gene_count.get(species,0) + 1                        
+                        
+#            if args.debug:
+#                t4 = time.time()
+#                print(f"Reading and storing data for {file} [{species_gene_count[species]} proteins] took {t4-t3} seconds",file=sys.stderr)
     
     if args.debug:
         t1 = time.time()
@@ -63,57 +72,79 @@ def main():
         print(f"Reading gene files took {total} seconds",file=sys.stderr)
     
     # Read in the cluster file
-    groups = []
     pairwise = {}
     clusterID = 0
     last_seq = ""
     last_cluster = set()
+    i = 0
+    t5 = time.time()
+    t4 = t5
     for line in args.input:
         if line.startswith('#'): continue
         cluster = line.strip().split()
         if len(cluster) != 2: continue
+        if args.debug:
+            if i % 1000000 == 0:
+                t6 = time.time()
+                print(f"Processing line {i} took {t6-t5} seconds; ClusterID is {clusterID}",file=sys.stderr)
+                t5 = t6
+            i+=1
         for gene in cluster:
-            if gene not in gene2species:
-                print(f"Gene {gene} not found in species list",file=sys.stderr)
-                continue
+            if db:
+                temp_species = db.get(gene.encode())
+                if not temp_species:
+                    print(f"Gene {gene} not found in species list",file=sys.stderr)
+                    continue
+            else:
+                if gene not in gene2species:
+                    print(f"Gene {gene} not found in species list",file=sys.stderr)
+                    continue
         gene1 = cluster[0]
         gene2 = cluster[1]
         if gene1 != last_seq and last_seq != "":
             if len(last_cluster) > 1:
-                #print(f"Cluster {clusterID}:")
-                #print(last_cluster)
                 names = []
                 species_grouping = {}
                 for item in last_cluster:
-                    species = gene2species[item]
+                    if db:
+                        species = db.get(item.encode()).decode()
+                    else:
+                        species = gene2species[item]
+                    
                     if species not in species_grouping:
                         species_grouping[species] = []
                     species_grouping[species].append(item)                
                 
                 for species1 in species_grouping:
                     if species1 not in pairwise:
-                        pairwise[species1] = bsddb3.btopen(os.path.join(args.tmpdir,f'{species1}.bdb'))
-                    sp1_orthologs = ", ".join(species_grouping[species1])                    
+                        fh = gzip.open(f"{args.outdir}/{species1}_orthologs.tsv.gz",'wt')
+                        if fh:
+                            writer = csv.writer(fh,delimiter='\t',lineterminator=os.linesep)
+                            writer.writerow(['Cluster','Species',species1,'Orthologs'])
+                            pairwise[species1] = {'handle': fh, 'csv': writer }
+                        else:
+                            print(f"Could not open {args.outdir}/{species1}_orthologs.tsv",file=sys.stderr)
+                            sys.exit(1)
+                    sp1_orthologs = ", ".join(species_grouping[species1])
                     for species2 in species_grouping:
                         if species1 == species2: continue
-                        sp2_orthologs = ", ".join(species_grouping[species2])    
-                        pairwise[species1].append([f'{args.prefix}{clusterID:0>8}',species2,
-                                                sp1_orthologs,sp2_orthologs])
-                groups.append(last_cluster)
+                        sp2_orthologs = ", ".join(species_grouping[species2])
+                        pairwise[species1]['csv'].writerow([f'{args.prefix}{clusterID:0>8}',
+                                                            species2,sp1_orthologs,
+                                                            sp2_orthologs])                
                 clusterID += 1
+
             last_cluster = set()
         last_cluster.add(gene1)
         last_cluster.add(gene2)
         last_seq = gene1
 
-    print(f"{len(groups)} clusters found",file=sys.stderr)
-    for sp in pairwise:
-        with open(f"{args.outdir}/{sp}_orthologs.tsv",'w') as f:
-            writer = csv.writer(f,delimiter='\t',lineterminator=os.linesep)
-            writer.writerow(['Cluster','Species',sp,'Orthologs'])
-            for row in pairwise[sp]:
-                writer.writerow(row)
-        
+    if args.debug:
+        t2 = time.time()
+        total = t2-t4
+        print(f"Processing clusters took {total} seconds",file=sys.stderr)
+    for d in pairwise.values():
+        d['handle'].close()
 
 if __name__ == "__main__":
     main()
