553e2b9a63cc479c64c6d8ef2d4143d889f200b5
Jason Stajich <jasonstajich.phd@gmail.com>
2025-01-19 16:43:05 -0800

    parallelize generation step

--- pipeline/function/07_sequence_stats.sh
+++ pipeline/function/07_sequence_stats.sh
@@ -10,26 +10,35 @@ fi
 module load biopython
 
 EXT=aa_freq.csv
-if [ ! -s bigquery/$EXT ]; then
+if [ ! -s bigquery/$EXT.gz ]; then
     INDIR=input
     
     ls -U $INDIR | grep \.fa | parallel -J $CPU ./scripts/calculate_AA_freq.py $INDIR/{} -v -o $SCRATCH/{.}.$EXT
     FIRST=$(ls -U $SCRATCH/*.$EXT | head -n 1)
     head -n 1 $FIRST > bigquery/$EXT
     ls -U $SCRATCH | grep -E "$EXT" | xargs -I {} sh -c "tail -n +2 $SCRATCH/{} >> bigquery/$EXT"
+    pigz -f bigquery/$EXT
 fi
 EXT=codon_freq.csv
-if [ ! -s bigquery/$EXT ]; then
+if [ ! -s bigquery/$EXT.gz ]; then
     INDIR=input_cds
     ls -U $INDIR | grep \.fa | parallel -J $CPU ./scripts/calculate_codon_freq.py $INDIR/{} -v -o $SCRATCH/{.}.$EXT
     FIRST=$(ls -U $SCRATCH/*.$EXT | head -n 1)
-    head -n 1 $FIRST > bigquery/c$EXT
+    head -n 1 $FIRST > bigquery/$EXT
     ls -U $SCRATCH | grep "$EXT" | xargs -I {} sh -c "tail -n +2 $SCRATCH/{} >> bigquery/$EXT.csv"
-#    ./scripts/calculate_codon_freq.py -d input_cds -o bigquery/codon_freq.csv
+    pigz -f bigquery/$EXT
 fi
 EXT=gene_info.csv
-if [ ! -s bigquery/$EXT ]; then
+if [ ! -s bigquery/$EXT.gz ]; then
     INDIR=gff3
-    # this should be fast enough
-    ./scripts/build_genestats_bigquery.py --gff_dir gff3
+    # do these in parallel
+    time ls -U $INDIR | grep \.gff3 | parallel -j $CPU ./scripts/build_genestats_bigquery.py $INDIR/{} --outdir $SCRATCH/{.}.${EXT}    
+    FIRST=$(ls -Ud $SCRATCH/*.${EXT} | head -n 1)
+    for FILE in $(ls $FIRST/*.csv); do
+        FNAME=$(basename $FILE)
+        head -n 1 $FILE > bigquery/$FNAME
+        ls -U $SCRATCH/ | grep "$EXT" | xargs -I {} sh -c "tail -n +2 $SCRATCH/{}/${FNAME} >> bigquery/${FNAME}"
+        pigz -f bigquery/${FNAME}
+    done
+
 fi
