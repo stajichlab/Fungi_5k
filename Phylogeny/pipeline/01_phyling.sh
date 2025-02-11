0d66fbfe797cf6d5d5221d39d11f2d486f7c2774
Jason Stajich <jasonstajich.phd@gmail.com>
2025-02-11 12:33:04 -0800

    run with verbose

--- Phylogeny/pipeline/01_phyling.sh
+++ Phylogeny/pipeline/01_phyling.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash -l
-#SBATCH -p epyc -c 48 -N 1 -n 1 --mem 256gb --out logs/phyling_align.log
+#SBATCH -c 96 -N 1 -n 1 --mem 256gb --out logs/phyling_align.log
 
 module load phyling
-CPU=2
+CPU=96
 if  [ $SLURM_CPUS_ON_NODE ]; then
  CPU=$SLURM_CPUS_ON_NODE
 fi
-phyling align -I input -m fungi_odb10 -t $CPU -o phyling_align -v
+phyling align -I input -m fungi_odb10 -t $CPU -o fungi_phyling_align -v 
 
