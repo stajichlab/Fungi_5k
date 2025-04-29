b7bd300a53694b18d773de198300ea6d6859f38d
Jason Stajich <jasonstajich.phd@gmail.com>
2025-04-28 21:58:43 -0700

    summarize the IDP / IDR

---
+++ pipeline/function/08_summarize_idp.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash -l
+#SBATCH -N 1 -n 1 -c 96 --mem 96gb -p short
+#SBATCH --job-name=aiupred_sum
+#SBATCH --output=logs/aiupred_summarize.log
+
+CPU=1
+if [ ! -z $SLURM_CPUS_ON_NODE ]; then
+    CPU=$SLURM_CPUS_ON_NODE
+fi
+
+EXT=idp.csv
+if [ ! -s bigquery/$EXT.gz ]; then
+    INDIR=results/function/aiupred
+    
+    ls -U $INDIR | grep \.aiupred\.txt\.gz | parallel -J $CPU ./scripts/gather_AIUPred.py $INDIR/{} -o $SCRATCH/{.}.$EXT
+    FIRST=$(ls -U $SCRATCH/*.$EXT | head -n 1)
+    head -n 1 $FIRST > bigquery/$EXT
+    ls -U $SCRATCH | grep -E "$EXT" | xargs -I {} sh -c "tail -n +2 $SCRATCH/{} >> bigquery/$EXT"
+    pigz -f bigquery/$EXT
+fi
